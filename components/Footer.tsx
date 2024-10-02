0e5b294079ece3e3e80ef6fa77cbe0a93a829cd5
zelshahawy <ziadelshahawygit@gmail.com>
2024-10-01 20:33:29 -0500

    added footer componenet

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="mt-8 text-center">
+            <p className="text-sm text-gray-500">Note: Image generation can be a little slow for the first image.</p>
+        </footer>
+    );
+};
+
+export default Footer;
