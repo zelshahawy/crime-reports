6841fdcb4d2081a3e2037bce6b36f4c29f140a88
Ziad Elshahawy <146673576+zelshahawy@users.noreply.github.com>
2024-10-09 09:01:08 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
         <footer className="mt-8 text-center">
-            <p className="text-sm text-gray-500">Note: Image generation can be a little slow for the first image.</p>
+            <p className="text-sm text-gray-500">Note: Image generation can be slow for the first image. If not image is shown, try toggling between filters.</p>
         </footer>
     );
 };
