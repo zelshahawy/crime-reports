6c9ed4fa3ed4920af16a6a10ca08c1690072fe16
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2025-01-26 16:17:24 -0600

    forced description

--- components/Search.tsx
+++ components/Search.tsx
@@ -13,7 +13,7 @@ const Search: React.FC<SearchProps> = ({ onSearch }) => {
         defaultValue=""
       >
         <option value="" disabled>Select a crime type...</option>
-        <option value="FORCED_SEX">Forced Sex</option>
+        <option value="FORCED_SEX">Forced Sexual Assault</option>
         <option value="VEHICLE_THEFT">Vehicle Theft</option>
         <option value="BROKEN_IN">Broken In</option>
         <option value="TOTAL_CRIME">Total of the three crimes</option>
