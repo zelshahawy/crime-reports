6308ba53f562e0850558d38c29362e4d60d01d71
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2025-01-28 17:32:04 -0600

    provded clearer description

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ const Header: React.FC = () => {
       <p className="text-lg mt-2">
         Analyze crime data from the National Crime Victimization Survey (NCVS) with filters and visualizations.
       </p>
-      <p className="text-lg mt-2"> A real-time tool that automatically scrapes for updated crime data to provide you with real & vital information anytime, anyday. </p>
+      <p className="text-lg mt-2"> A real-time tool that automatically scrapes for anually updated crime data to provide you with real & vital information anytime, anyday. </p>
     <p className='text-lg mt-2'> Note that NCVS only interview victims of such crime, and this is not a random sruvey done on the US population.</p>
     </header>
   );
